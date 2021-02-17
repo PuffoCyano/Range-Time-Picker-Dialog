e164c55f5385133d2ee8cb2197812f1c1ad5b358
ynotawino <ynotomondi57@gmail.com>
2021-02-17 11:18:35 +0300

    Update gradle, migrate to Android X, switch positive and negative button positions

--- app/src/main/java/com/mcsoft/exampleapprangetimepickerdialog/MainActivity.java
+++ app/src/main/java/com/mcsoft/exampleapprangetimepickerdialog/MainActivity.java
@@ -2,9 +2,9 @@ package com.mcsoft.exampleapprangetimepickerdialog;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
